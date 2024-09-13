eedb860ed5b2e1a3c650521a5f1a3819715ae75a
Fabio Sierra Redondo <fabiosierra281@gmail.com>
2024-09-13 06:33:33 -0500

    verficacion

--- Conjunto/Persistencia/login.cs
+++ Conjunto/Persistencia/login.cs
@@ -9,6 +9,28 @@ namespace Persistencia
 {
     public class login
     {
-       
+        private Conexion conexion = new Conexion();
+
+        private SqlCommand command = new SqlCommand();
+        private SqlDataReader reader = null;
+        private bool n = false;
+
+        public bool VerificarUsuario(string email, string contraseña)
+        {
+            command.CommandText = "SPSelecionarUsuario";
+            command.Connection = conexion.AbrirConexion();
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            reader = command.ExecuteReader();
+            command.Parameters.Clear();
+
+            while (reader.Read()) {
+                if (reader[2].ToString() == email && reader[3].ToString() == contraseña)
+                {
+                    n = true; break;
+                }
+            }
+
+            return n;
+        }
     }
 }
