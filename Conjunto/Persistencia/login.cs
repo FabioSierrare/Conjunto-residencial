825f5a309a105ff5540a150277b583225633be15
Fabio Sierra Redondo <fabiosierra281@gmail.com>
2024-09-13 08:01:00 -0500

    registro

--- Conjunto/Persistencia/login.cs
+++ Conjunto/Persistencia/login.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Persistencia
@@ -30,7 +33,24 @@ namespace Persistencia
                 }
             }
 
+            conexion.CerrarConexion();
+
             return n;
+        }
+
+        public void Registro(string usuario, string email, string contraseña, string tipo_documento, string numero_documento)
+        {
+            command.Connection = conexion.AbrirConexion();
+            command.CommandText = "spCrearUsuario";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@usuario",usuario);
+            command.Parameters.AddWithValue("@correo", email);
+            command.Parameters.AddWithValue("@contraseña", contraseña);
+            command.Parameters.AddWithValue("@tipo_documento", tipo_documento);
+            command.Parameters.AddWithValue("@numero_documento", numero_documento);
+            command.ExecuteNonQuery();
+            command.Parameters.Clear();
+            conexion.CerrarConexion();
         }
     }
 }
