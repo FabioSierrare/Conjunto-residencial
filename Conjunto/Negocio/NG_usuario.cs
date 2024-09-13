bc196d464cb6bc29f2159118d815ab8912615de2
Fabio Sierra Redondo <fabiosierra281@gmail.com>
2024-09-13 06:48:47 -0500

    negocio verificar

---
+++ Conjunto/Negocio/NG_usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class NG_usuario
+    {
+        private login log = new login();
+        bool n = false;
+
+        public bool VerificarUser(string email, string contraseña)
+        {
+            n = log.VerificarUsuario(email, contraseña);
+            return n;
+        }
+    }
+}
